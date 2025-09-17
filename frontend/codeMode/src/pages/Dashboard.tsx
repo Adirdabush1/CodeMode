a66e364402ab1ab68d81a3cee61f139f325efe88
Adirdabush1 <adirdabosh33@gmail.com>
2025-09-17 11:15:13 +0300

    new Dash1

--- frontend/codeMode/src/pages/Dashboard.tsx
+++ frontend/codeMode/src/pages/Dashboard.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import MenuBar from "../components/MenuBar";
-import "./Dashboard.css";
+import "./Dashbord.css";
 
 type UserSummary = {
   id?: string;
